be89a0ac3bb034b337d2ffacf8f8c794f2c26029
daffaalam <daffaalam@gmail.com>
2018-01-09 14:19:51 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/rbs/projectjanuari/InputBiodataActivity.java
@@ -0,0 +1,13 @@
+package id.sch.rbs.projectjanuari;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputBiodataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_biodata);
+    }
+}
